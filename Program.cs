a8b2364a3003d6b1e1cf5b7290e0b64fc51c3cb4
gregnani <fgregnani@gmail.com>
2023-04-02 16:19:32 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿decimal valorcompra, valorpago, valortroco;
+
+Console.WriteLine("...Troco...\n");
+
+Console.Write("Valor da compra (R$)...:\n");
+Console.ForegroundColor = ConsoleColor.Blue;
+valorcompra = Convert.ToDecimal(Console.ReadLine());
+
+Console.ResetColor();
+
+Console.Write("Valor pago (R$)...: ");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+valorpago = Convert.ToDecimal(Console.ReadLine());
+
+Console.ResetColor();
+
+valortroco = valorpago - valorcompra;
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"\nTroco: {valortroco:c}");
+
+Console.ResetColor();
